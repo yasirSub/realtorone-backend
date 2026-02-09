24448bf6fa6a5107a92b860c4f063f7e63357142
Yasir Subhani <yasir.subhani123@gmail.com>
2026-02-09 14:45:45 +0530

    fix: import Hash facade in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
